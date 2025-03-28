189cc8860076d05faba3c2ca1391c03bd21b2bf2
Kaibolt <kyle.salowitz08@gmail.com>
2025-03-28 12:32:24 -0400

    a

---
+++ app/src/assets/keyFunctions.js
@@ -0,0 +1,4 @@
+export function randomNumber(maximum) {
+    const number = Math.floor(Math.random() * maximum + 1);
+    return number;
+  }
--- app/src/components/TestDice.vue
+++ app/src/components/TestDice.vue
@@ -1,29 +1,31 @@
+<!--
 <template>
     <div>
-        <div @click="testClick(diceMulti)" id="hihi">Hi</div>
+        <div @click="testClick()" id="hihi">Hi</div>
         <div @click="upgradeClick"id="byebye">Bye</div>
     </div>
 </template>
 
 <script setup>
-function randomNumber(maximum) {
-  const number = Math.floor(Math.random() * maximum + 1);
-  return number;
-}
+import { randomNumber } from '../assets/keyFunctions.js'
 let testCash = 0
 let diceMulti=1
 console.log(testCash)
-function testClick(diceMulti){
+function testClick(){
     let x = randomNumber(5)
     console.log("Base "+ x)
     let y = x*diceMulti
     console.log("Multiplied " + y)
+    console.log("Multiplier " + diceMulti)
     testCash+= y
     console.log("Cash: "+testCash)
 }
 function upgradeClick(){
     diceMulti +=1
     console.log("Dice Multiplier: " + diceMulti)
+    if (diceMulti == 10) {
+        console.log("hi")
+    }
 }
 
 </script>
@@ -35,4 +37,42 @@ function upgradeClick(){
 #byebye{
     background-color: red;
 }
-</style>
+</style> 
+Base Functions
+-->
+<template>
+    <div>
+        <div @click="testClick" id="hihi">Hi</div>
+        <div @click="upgradeClick" id="byebye">Bye</div>
+    </div>
+</template>
+
+<script>
+let testList = [
+    {name: "1", level: "1"}
+]
+function testClick(){
+    let diceMulti = testList[1].level
+    let x = randomNumber(5)
+    let y = x*diceMulti
+    testCash+= y
+    console.log("Cash: "+testCash)
+}
+function upgradeClick(){
+    testList[1].level +=1
+    console.log("Dice Multiplier: " + testList[1].level)
+    if (testList[1].level == 10) {
+        console.log("hi")
+    }
+}
+console.log(testList)
+</script>
+
+<style scoped>
+#hihi{
+    background-color: blue;
+}
+#byebye{
+    background-color: red;
+}
+</style> 
