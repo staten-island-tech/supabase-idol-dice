bd47470fb905647db73c9bdbe05cf6b5dc69682b
RBJ40 <celinechan268@gmail.com>
2025-04-04 12:30:02 -0400

    draft

--- app/src/components/TestDice.vue
+++ app/src/components/TestDice.vue
@@ -41,38 +41,43 @@ function upgradeClick(){
 Base Functions
 -->
 <template>
-    <div>
-        <div @click="testClick" id="hihi">Hi</div>
-        <div @click="upgradeClick" id="byebye">Bye</div>
-    </div>
+  <div>
+    <div @click="testClick" id="hihi">Hi</div>
+    <div @click="upgradeClick" id="byebye">Bye</div>
+  </div>
 </template>
 
 <script>
-let testList = [
-    {name: "1", level: "1"}
-]
-function testClick(){
-    let diceMulti = testList[1].level
-    let x = randomNumber(5)
-    let y = x*diceMulti
-    testCash+= y
-    console.log("Cash: "+testCash)
+import { gsap } from 'gsap'
+let testList = [{ name: '1', level: '1' }]
+function testClick() {
+  let diceMulti = testList[1].level
+  let x = randomNumber(5)
+  let y = x * diceMulti
+  testCash += y
+  console.log('Cash: ' + testCash)
 }
-function upgradeClick(){
-    testList[1].level +=1
-    console.log("Dice Multiplier: " + testList[1].level)
-    if (testList[1].level == 10) {
-        console.log("hi")
-    }
+function upgradeClick() {
+  testList[1].level += 1
+  console.log('Dice Multiplier: ' + testList[1].level)
+  if (testList[1].level == 10) {
+    console.log('hi')
+  }
 }
 console.log(testList)
+
+gsap.to(div, {
+  xPercent: 50,
+  yPercent: 50,
+}) //It works. Still trying to figure out how it works though.
+gsap.to('#hihi', { x: 200 })
 </script>
 
 <style scoped>
-#hihi{
-    background-color: blue;
+#hihi {
+  background-color: blue;
 }
-#byebye{
-    background-color: red;
+#byebye {
+  background-color: red;
 }
-</style> 
+</style>
