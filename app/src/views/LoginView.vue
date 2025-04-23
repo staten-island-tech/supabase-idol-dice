cfa0fe5ef44dfbeeae20bfcc718a0a4e4a72628f
RBJ40 <celinechan268@gmail.com>
2025-04-23 12:30:56 -0400

    draft

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue'
-//import { supabase } from "../clients.supabase"
+import { supabase } from '../components/icons/lib/supabaseClient'
 
 let email = ref('')
 let password = ref('')
+let firstName = ref('')
 
 async function createAccount() {
   const { data, error } = await supabase.auth.signUp({
@@ -17,8 +18,17 @@ async function createAccount() {
     console.log(data)
   }
 }
+
 async function signIn() {
-  const { data, error } = await supabase.auth.signInWithPassword()
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value,
+  })
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(data)
+  }
 }
 function logOut() {
   console.log('Logout')
@@ -26,13 +36,32 @@ function logOut() {
 </script>
 
 <template>
-  <div class="links">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/login">Login</RouterLink>
-    </nav>
+  <h1>Test Form</h1>
+
+  <div class="inputContainer">
+    <label for="email"> Email: </label>
+    <input type="email" id="email" v-model="email" />
   </div>
-  <div class="content">
-    <RouterView />
+
+  <div class="inputContainer">
+    <label for="email"> Password: </label>
+    <input type="password" id="password" v-model="password" />
+  </div>
+
+  <div class="inputContainer">
+    <label for="firstName"> First Name </label>
+    <input type="firstName" id="firstName" v-model="firstName" />
+  </div>
+
+  <div class="buttonContainer">
+    <button @click="createAccount()">Create</button>
+    <button @click="signIn()">Login</button>
+    <button @click="logOut()">Logout</button>
   </div>
 </template>
+
+<style>
+.buttonContainer {
+  display: flex;
+}
+</style>
