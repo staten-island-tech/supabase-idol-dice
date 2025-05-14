a8ca33f2023468c10b57cf85f9d6ef7ae8e1e1ad
Kaibolt <kyle.salowitz08@gmail.com>
2025-05-14 12:28:26 -0400

    Quality of Life
    Also known as delaying the inevitable problems. This will make pinia easier to implement as it will visibly show cash, which will help make sure it's a global component.
    Next: Pinia; Saving; Routes; Inevitable array problem

--- app/src/components/TestDice.vue
+++ app/src/components/TestDice.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <div @click="testClick" id="hihi">Hi</div>
-        <div @click="upgradeClick" id="byebye">Bye</div>
+        <div @click="testClick" id="hihi">Roll dice</div>
+        <div @click="upgradeClick" id="byebye">Increase multiplier</div>
     </div>
-<h1>Cash: {{ tesst }}</h1>-
+<h1>Cash: {{ tesstCash }}</h1>
+<h1>You rolled a {{ tesstRoll }}</h1>
+<h1>Your multiplier is {{ diceMultiDisplay }}</h1>
 </template>
 
 <script setup>
@@ -13,18 +15,25 @@ import { randomNumber } from '@/assets/keyFunctions'
 // The final version of this list must start with 1 dice and expand when a button is clicked to add a new div and dice into the list.
 let testList = [{ name: '1', level: '1' }]
 
-let tesst = ref(0)
+let tesstCash = ref(0)
+let tesstRoll = ref(0)
 
-let diceMulti = testList[0].level // Right now, dice multiplier would affect all clicking on dice. It should only apply to the dice it is on.
+let diceMultiDisplay = ref(1)
+let diceMulti = 1 // Right now, dice multiplier would affect all clicking on dice. It should only apply to the dice it is on.
 let testCash = 0 // Everyone starts with 0 cash
 function testClick() {
   let x = randomNumber(5)
+  tesstRoll.value = x
   let y = x * diceMulti
   testCash += y
   console.log('Cash: ' + testCash)
   console.log('Level: ' + testList[0].level)
   console.log('Multi: ' + diceMulti) // Basic click function concept
-  tesst.value = testCash
+  tesstCash.value = testCash
+}
+function upgradeClick() {
+  diceMulti += 1
+  diceMultiDisplay.value = diceMulti
 }
 </script>
 <style scoped>
