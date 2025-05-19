dcc6562da78b53a5059079c688bb35675e350059
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2025-05-19 11:23:20 -0400

    Syntax
    Syntax is the bane of my existence. It is like grammar in Russian. It is a pain. A necessary, hateful pain.

--- app/src/components/TestDice.vue
+++ app/src/components/TestDice.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-        <div @click="testClick" id="hihi">Roll dice</div>
-        <div @click="upgradeClick" id="byebye">Increase multiplier</div>
-    </div>
-<h1>Cash: {{ displayCash }}</h1>
-<h1>You rolled a {{ displayRoll }}</h1>
-<h1>Your multiplier is {{ diceMultiDisplay }}</h1>
+  <div>
+    <div @click="store.testClick" id="hihi">Roll dice</div>
+    <div @click="store.upgradeClick" id="byebye">Increase multiplier</div>
+  </div>
+  <h1>Cash: {{ store.displayCash }}</h1>
+  <h1>You rolled a {{ store.displayRoll }}</h1>
+  <h1>Your multiplier is {{ store.diceMultiDisplay }}</h1>
 </template>
 
 <script setup>
-import { useMoneyStore } from '@/stores/money';
-const {displayCash, displayRoll, diceMultiDisplay, upgradeClick, testClick} = store
+import { useMoneyStore } from '@/stores/money'
+const store = useMoneyStore()
 
 // The final version of this list must start with 1 dice and expand when a button is clicked to add a new div and dice into the list.
 //        let testList = [{ name: '1', level: '1' }]
-
-
 </script>
 <style scoped>
 #hihi {
