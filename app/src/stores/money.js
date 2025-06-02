2e4af0c6846c7cc28568cdcc504f846ebc437566
Kaibolt <kyle.salowitz08@gmail.com>
2025-06-02 12:16:50 -0400

    Checkpoint (Read Desc)
    Creates table information unique to user id for each click.
    Next:
    When user logs in, retrieve the data from the table corresponding to their user ID. If that doesn't exist, make a table.
    Set that data to the used data in the store, such as the table money becoming current money for rolls.
    Then, clicking the roll will replace the old with the new.

--- app/src/stores/money.js
+++ app/src/stores/money.js
@@ -4,14 +4,19 @@ import { randomNumber } from '@/assets/keyFunctions'
 import { useAuthStore } from '@/stores/authenticate'
 import { supabase } from '../lib/supabaseClient.js'
 const store = useAuthStore()
-
+const updateChart = () => {}
 export const useMoneyStore = defineStore('money', () => {
-  async function testTable() {
-    await supabase.from('information').insert([
+  async function testTable(x) {
+    const {error} = await supabase.from('information').insert([
    {
      name: store.userData.user.id,
-     money: 0
-   }])}
+     money: x
+   }])
+  if (error){console.log(error)}
+    else {
+  updateChart()
+}
+  }
   let displayCash = ref(0)
   let displayRoll = ref(0)
   console.log(store.userData.user)
@@ -19,7 +24,6 @@ export const useMoneyStore = defineStore('money', () => {
   let diceMulti = 1 // Right now, dice multiplier would affect all clicking on dice. It should only apply to the dice it is on.
   // base cash
   function testClick() {
-     testTable() // Potential Issue
     console.log(store.userData.user.id)
     let x = randomNumber(5)
     displayRoll.value = x
@@ -27,6 +31,7 @@ export const useMoneyStore = defineStore('money', () => {
     displayCash.value += y
     console.log('Cash: ' + displayCash)
     console.log('Multi: ' + diceMulti) // Basic click function concept
+    testTable(displayCash.value) // Potential Issue
   }
   function upgradeClick() {
     diceMulti += 1
