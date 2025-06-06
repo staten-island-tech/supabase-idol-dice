d1b5c45402bb0c8d8d9b259d1df3b9daa62c9b7b
RBJ40 <celinechan268@gmail.com>
2025-06-06 12:30:44 -0400

    draft

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -4,6 +4,7 @@ const router = useRouter()
 import { ref } from 'vue'
 import { supabase } from '../components/icons/lib/supabaseClient'
 import { useAuthStore } from '../stores/authenticate'
+import { RouterLink, RouterView } from 'vue-router'
 //import { LoadingScreenAnimation } from '../app/public/LoadingScreenAnimation'
 let email = ref('')
 let password = ref('')
@@ -50,55 +51,63 @@ function testData() {
 </script>
 
 <template>
-  <div class="dropdown">
-    <button onclick="myFunction()" class="dropbtn">Menu</button>
-    <div id="myDropdown" class="dropdown-content">
-      <a href="#">Home</a>
-      <a href="#">Store</a>
-      <a href="#">Link 3</a>
+  <body>
+    <div class="dropdown">
+      <button onclick="myFunction()" class="dropbtn">Menu</button>
+      <div id="myDropdown" class="dropdown-content">
+        <a href="#">Home</a>
+        <a href="#">Store</a>
+        <a href="#">Play</a>
+        <a href="#">Prestige</a>
+      </div>
+      <RouterLink to="/PrestigeView"></RouterLink>
     </div>
-  </div>
-  <div class="inputContainer">
-    <label for="email"> Email: </label>
-    <input type="email" id="email" v-model="email" />
-    <label for="email"> Password: </label>
-    <input type="password" id="password" v-model="password" />
-  </div>
-  <div class="buttonContainer">
-    <button @click="createAccount()">Create</button>
-    <button @click="signIn()">Login</button>
-    <button @click="logOut()">Logout</button>
-    <button @click="testData()">User Data</button>
-  </div>
-  <!-- <body></body> -->
-  <div id="svgContainer">
-    <object type="image/svg+xml" data="/public/LoadingScreenAnimation.svg">
-      <img src="/public/LoadingScreenAnimation.svg" />
-    </object>
-    <object type="image/svg+xml" data="/public/BuyAnimation.svg">
-      <img src="/public/BuyAnimation.svg" />
-    </object>
-  </div>
+    <div class="inputContainer">
+      <label for="email"> Email: </label>
+      <input type="email" id="email" v-model="email" />
+      <label for="email"> Password: </label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <div class="buttonContainer">
+      <button @click="createAccount()">Create</button>
+      <button @click="signIn()">Login</button>
+      <button @click="logOut()">Logout</button>
+      <button @click="testData()">User Data</button>
+    </div>
+    <!-- <body></body> -->
+    <div id="svgContainer">
+      <object type="image/svg+xml" data="/public/LoadingScreenAnimation.svg">
+        <img src="/public/LoadingScreenAnimation.svg" />
+      </object>
+      <object type="image/svg+xml" data="/public/BuyAnimation.svg">
+        <img src="/public/BuyAnimation.svg" />
+      </object>
+    </div>
+  </body>
 </template>
 
 <style>
+body {
+  display: flex;
+}
 .dropbtn {
   background-color: #b6cad4;
   color: rgb(0, 0, 0);
   padding: 7px;
   font-size: 8px;
   cursor: pointer;
+  display: flex;
 }
 .dropdown {
-  position: absolute;
-  display: inline-block;
+  display: flex;
 }
 .container {
-  display: inline-block;
+  display: flex;
   cursor: pointer;
 }
 h1 {
   text-decoration: underline;
+  display: flex;
 }
 .inputContainer {
   display: flex;
