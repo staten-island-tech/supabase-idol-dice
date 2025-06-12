a2e56d62cdbe243da6a0ada8a94eb4f51d0c53e0
Kaibolt <kyle.salowitz08@gmail.com>
2025-06-12 11:36:29 -0400

    Update BuyAnimation.vue

---
+++ app/src/components/BuyAnimation.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="svgContainer">
+      <!--rip-->
+      <object type="image/svg+xml" data="/public/BuyAnimation.svg">
+        <img src="/public/BuyAnimation.svg" />
+      </object>
+      </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+setTimeout(() => {
+   router.push()
+}, timeout);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
