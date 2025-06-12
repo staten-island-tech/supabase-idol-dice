540101bf8b4a6387be3030056b956e0927d46653
Kaibolt <kyle.salowitz08@gmail.com>
2025-06-12 11:45:01 -0400

    Update PrestigeAnim.vue

---
+++ app/src/components/PrestigeAnim.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>This page will have the prestige animation</h1>
+    </div>
+</template>
+
+<script setup>
+// This script will have the timer
+</script>
+
+<style lang="scss" scoped>
+
+</style>
